4cc218efdd4fc7bff4e3c98b440ac1a0177ce6f1
alessandra <alessandrasilvav@gmail.com>
2022-06-14 16:58:09 -0300

    subir desafio

---
+++ Desafio.ts
@@ -0,0 +1,98 @@
+const funcionario = {
+    codigo: 10,
+    nome: 'João'
+};
+
+const funcionario2: {codigo: number, nome: string} = {
+    codigo: 10,
+    nome: 'joao'
+}
+
+interface Funcionario {
+    codigo: number,
+    nome: string
+};
+
+const funcionarioObj = {} as Funcionario;
+funcionarioObj.codigo = 10;
+funcionarioObj.nome = 'João';
+
+const funcionarioObj2: Funcionario = {
+    codigo: 10,
+    nome: 'João'
+}
+
+
+enum Trabalho {
+    Atriz,
+    Padeiro
+}
+
+type Humano = {
+    nome: string,
+    idade: number,
+    profissao: Trabalho
+}
+
+let pessoa1: Humano = {
+    nome: 'maria',
+    idade: 29,
+    profissao: Trabalho.Atriz
+};
+
+let pessoa2: Humano = {
+    nome: 'roberto',
+    idade: 19,
+    profissao: Trabalho.Padeiro
+};
+
+let pessoa3: Humano = {
+    nome: 'laura',
+    idade: 32,
+    profissao: Trabalho.Atriz
+};
+
+let pessoa4: Humano = {
+    nome: "carlos",
+    idade: 19,
+    profissao: Trabalho.Padeiro
+}
+
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo');
+
+let saldoTotal = 0
+
+limparSaldo()
+
+function somarAoSaldo(soma: number) {
+    if (campoSaldo) {
+        saldoTotal += soma
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+
+function limparCampoSoma() {
+    soma.value = "";
+}
+
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', () => {
+        somarAoSaldo(Number(soma.value)); 
+    });
+}
+botaoLimpar.addEventListener('click', () => { 
+    limparSaldo();
+});
+
+
